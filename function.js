fe039b54b53860019ec37ec33249bcf8f6261b1b
Ismael Chaves Mizael <125465461+Ismael-Oficial@users.noreply.github.com>
2023-07-19 17:04:31 -0300

    Add files via upload

--- function.js
+++ function.js
@@ -2,13 +2,17 @@ function lucro() {
 
     var valor = document.getElementById('valor')
     var percentual = document.getElementById('percentual')
-    var result = document.getElementById('resultado')
+    var result = document.getElementById('resultado1')
     var result2 = document.getElementById('resultado2')
+    var result3  = document.getElementById('resultado3')
+    var result4  = document.getElementById('resultado4')
 
     var valor = Number(valor.value)
     var percentual = Number(percentual.value)
     var soma = parseFloat((percentual / 100) * valor)
-    result.innerHTML = "O Lucro será de: R$" + soma
 
-    result2.innerHTML = "O Preço sugerido é R$" + (valor + soma)
+    result.innerHTML = "Valor de Lucro: R$" + soma
+    result2.innerHTML = "Preço sugerido R$" + (valor + soma)
+    result3.innerHTML = "Percentual do calculo: " + percentual + "%"
+    result4.innerHTML = "O Custo inicial foi de R$" + valor
 }
